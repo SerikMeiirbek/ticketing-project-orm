4ea3fd27a07e8689967937144933eb305dc83508
serikmeiirbek <serik.meiirbek@gmail.com>
2022-06-21 10:36:34 -0400

    Part5

--- src/main/java/com/cydeo/mapper/ProjectMapper.java
+++ src/main/java/com/cydeo/mapper/ProjectMapper.java
@@ -1,9 +1,7 @@
 package com.cydeo.mapper;
 
 import com.cydeo.dto.ProjectDTO;
-import com.cydeo.dto.RoleDTO;
 import com.cydeo.entity.Project;
-import com.cydeo.entity.Role;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
