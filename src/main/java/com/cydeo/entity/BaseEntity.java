993741ee63be93bd0225abfc4504451ec52a0453
serikmeiirbek <serik.meiirbek@gmail.com>
2023-02-28 20:30:00 -0500

    Deleted Crud Service and AbstractMapService.java

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -2,15 +2,23 @@ package com.cydeo.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
+@MappedSuperclass
+@AllArgsConstructor
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime insertDateTime;
     private Long insertUserId;
